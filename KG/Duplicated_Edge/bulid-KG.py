70ad540aab41b01044c2b4beb1336844b846dc9b
Rebecia <2513501067@qq.com>
2025-03-27 15:03:01 +0800

    update RQ1

--- KG/Duplicated_Edge/bulid-KG.py
+++ KG/Duplicated_Edge/bulid-KG.py
@@ -20,13 +20,12 @@ def append_kg_to_json(graph, filename):
 
 def bulid_kg():
 
-    excel_path = ''  
-    sheet_name = ''        
+    excel_path = 'Data collection/malware/malware.xlsx'  
+    sheet_name = 'RubyGems'        
     # df = pd.read_csv(excel_path)
     df = pd.read_excel(excel_path,sheet_name=sheet_name)
-    # df = df[3896:]
-    graph = nx.Graph()
 
+    graph = nx.Graph()
     node_count = 0
 
     for index, row in tqdm(df.iterrows(), total=len(df), desc="Building Knowledge Graph"):
@@ -34,10 +33,11 @@ def bulid_kg():
             "package_id": row['ID'],
             "OSS": row['OSS'],
             "Source": row['Source'],
-            "Status": row['Status'],   
+            "Status": row['Status'],
             "package_name": row['name_version'],
 
             "edge_name": list(),
+
         }
 
         for key, value in node_data.items():
@@ -56,10 +56,13 @@ def bulid_kg():
                     graph.nodes[row['ID']].setdefault('edge_name', []).append(other_row['ID'])
                     graph.nodes[other_row['ID']].setdefault('edge_name', []).append(row['ID'])
 
+    # save as json
     json_data = nx.node_link_data(graph)
     json_str = json.dumps(json_data, indent=4, default=handle_non_serializable)
-    with open('./graph_name.json', 'w', encoding='utf-8') as json_file:
+    with open('./ruby_graph_name.json', 'w', encoding='utf-8') as json_file:
         json_file.write(json_str)
+
+    print(f"Knowledge Graph Included {node_count} nodes")
 
 
 bulid_kg()
